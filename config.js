8c555b63284e2f83cc476aad1e82c12dd9e671f6
Waehasun Leema <waehasunleema@Depas-MacBook-Air-3.local>
2026-08-16 19:32:05 +0700

    Connect water monitoring API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.AQUA_CONFIG = {
   // วาง URL Web app ของ Google Apps Script ที่ลงท้ายด้วย /exec
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbw214Ca1EJy75U-5jkLb4mz5_rIME8Gq7orMxaH0AJB-n4GJR0EFc7fbhWjb_wT7vOC/exec",
   refreshEveryMs: 60_000,
   staleAfterMs: 10 * 60_000,
 };
